727a798be6eebe6f54b9808633766b64e5a05bc7
Nikolay Bondarchuk <nbondarchuk@list.ru>
2023-05-02 12:58:13 +0300

    A solution to problem 70.

---
+++ src/math/ClimbingStairs_70/Main.java
@@ -0,0 +1,11 @@
+package math.ClimbingStairs_70;
+
+/**
+ * <a href="https://leetcode.com/problems/climbing-stairs/description/">Climbing Stairs</a>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().climbStairs(4));
+    }
+}
---
+++ src/math/ClimbingStairs_70/Solution.java
@@ -0,0 +1,32 @@
+package math.ClimbingStairs_70;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    private int maxN;
+
+    private final List<Integer> values = new ArrayList<>();
+
+    public Solution() {
+        maxN = 2;
+        values.add(1);
+        values.add(2);
+    }
+
+    public int climbStairs(int n) {
+        if (n > maxN) {
+            for (int i = maxN + 1; i <= n; i++) {
+                values.add(get(i - 1) + get(i - 2));
+            }
+            maxN = n;
+        }
+        return get(n);
+    }
+
+    // n starts from 1, but list indexes are zero-based
+    private int get(int n) {
+        return values.get(n - 1);
+    }
+}
