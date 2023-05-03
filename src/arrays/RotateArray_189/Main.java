d0b3f18eb337bbe364a9d6523fc1493bb610f7be
Nikolay Bondarchuk <nbondarchuk@list.ru>
2023-05-03 22:28:14 +0300

    A solution to problem 189.

---
+++ src/arrays/RotateArray_189/Main.java
@@ -0,0 +1,11 @@
+package arrays.RotateArray_189;
+
+/**
+ * <a href="https://leetcode.com/problems/rotate-array/">Rotate Array</a>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new Solution().rotate(new int[] {1,2,3,4,5,6,7}, 3);
+    }
+}
---
+++ src/arrays/RotateArray_189/Solution.java
@@ -0,0 +1,23 @@
+package arrays.RotateArray_189;
+
+public class Solution {
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+}
