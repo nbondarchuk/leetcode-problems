c3980585a3e03bd99583ba4db9eaa11a856d3427
Nikolay Bondarchuk <nbondarchuk@list.ru>
2023-05-10 11:44:42 +0300

    A solution to problem 204.

---
+++ src/math/CountPrimes_204/Main.java
@@ -0,0 +1,14 @@
+package math.CountPrimes_204;
+
+/**
+ * <a href="https://leetcode.com/problems/count-primes/">Count Primes</a>
+ * <p>
+ *     Given an integer n, return the number of prime numbers that are strictly less than n.
+ * </p>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().countPrimes(30));
+    }
+}
---
+++ src/math/CountPrimes_204/Solution.java
@@ -0,0 +1,34 @@
+package math.CountPrimes_204;
+
+/**
+ * See <a href="https://ru.wikipedia.org/wiki/Решето_Эратосфена">Sieve of Eratosthenes</a>
+ */
+class Solution {
+
+    public int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+
+        boolean[] primes = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            primes[i] = true;
+        }
+
+        for (int i = 2; i * i < n; i++) {
+            if (primes[i]) {
+                for (int j = i * i; j < n; j = j + i) {
+                    primes[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (boolean prime : primes) {
+            if (prime) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
