9899073f2caf4a6cd9069a263b1d9ad585575035
Nikolay Bondarchuk <nbondarchuk@list.ru>
2023-06-10 17:52:06 +0300

    Some more solutions.

--- src/arrays/MoveZeroes_283/Main.java
+++ src/arrays/MoveZeroes_283/Main.java
@@ -1,7 +1,6 @@
 package arrays.MoveZeroes_283;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * <a href="https://leetcode.com/problems/move-zeroes/">Move Zeroes</a>
@@ -12,54 +11,9 @@ import java.util.List;
  */
 public class Main {
 
-    static class Person {
-
-        List<? super Person> children;
-
-        public List<? super Person> getChildren() {
-            return children;
-        }
-
-        void setChildren(List<Person> children) {
-        }
-    }
-
-    static class Boy extends Person {
-    }
-
-    static class Girl extends Person {
-    }
-
     public static void main(String[] args) {
-        List<Person> child = new ArrayList<>();
-        child.add(new Boy());
-        child.add(new Girl());
-
-        new Person().setChildren(child);
-        new Person().setChildren(List.of(new Boy(), new Girl()));
-
-        try {
-            m1();
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("1");
-            throw new NullPointerException();
-        } catch (NullPointerException e) {
-            System.out.println("2");
-        } catch (Exception e) {
-            System.out.println("3");
-        } finally {
-            System.out.println("4");
-        }
-
-        System.out.println("end");
-
-//        int[] nums = {0, 1, 0, 3, 12};
-//        new Solution().moveZeroes(nums);
-//        System.out.println(Arrays.toString(nums));
-    }
-
-    private static void m1() {
-        System.out.println("m1");
-        throw new IndexOutOfBoundsException();
+        int[] nums = {0, 1, 0, 3, 12};
+        new Solution().moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
