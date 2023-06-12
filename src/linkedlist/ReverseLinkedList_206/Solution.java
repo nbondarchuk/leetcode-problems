7cb78fb20bb4a8d55f316945b1db280185bbb302
Nikolay Bondarchuk <nbondarchuk@list.ru>
2023-06-12 16:24:47 +0300

    Reverse list refactored.

--- src/linkedlist/ReverseLinkedList_206/Solution.java
+++ src/linkedlist/ReverseLinkedList_206/Solution.java
@@ -2,26 +2,16 @@ package linkedlist.ReverseLinkedList_206;
 
 import linkedlist.ListNode;
 
-public class Solution {
+class Solution {
 
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+        ListNode prev = null;
+        while (head != null) {
+            ListNode tempNext = head.next;
+            head.next = prev;
+            prev = head;
+            head = tempNext;
         }
-
-        ListNode newHead = head;
-        head = head.next;
-        newHead.next = null;
-
-        while (head.next != null) {
-            ListNode next = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = next;
-        }
-
-        head.next = newHead;
-        newHead = head;
-        return newHead;
+        return prev;
     }
 }
